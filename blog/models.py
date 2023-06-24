5c02b9be4285b3ed895169fab4ce12a2ee7200e3
jon_prog <jonashenri.aquino@gmail.com>
2023-06-24 03:06:37 -0300

    I put the styles and added new pages

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from utils.rands import slugify_new
 
-# Create your models here.
+class Tag(models.Model):
+    class Meta:
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
+    
+    name = models.CharField(max_length=150)
+    slug = models.SlugField(
+        unique = True, default = None,
+        null = True, blank = True, max_length = 150
+    )
+
+    def save(self,*args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.name, 4)
+        super().save(*args, **kwargs)
+
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categorys'
+    
+    name = models.CharField(max_length=150)
+    slug = models.SlugField(
+        unique = True, default = None,
+        null = True, blank = True, max_length = 150
+    )
+
+    def save(self,*args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.name, 4)
+        super().save(*args, **kwargs)
