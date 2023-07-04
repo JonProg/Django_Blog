ab44f627fa8e91d473673737e48dfa61b9415f61
jon_prog <jonashenri.aquino@gmail.com>
2023-07-04 15:09:30 -0300

    showing created posts in index.html

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from blog.models import Post
 
 def index(request):
+    posts = Post.objects.filter(is_published=True).order_by('-pk')
+
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     return render(
         request,
-        'blog/pages/index.html'
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+def page(request):
+    return render(
+        request,
+        'blog/pages/page.html',
+    )
+
+def post(request):
+    return render(
+        request,
+        'blog/pages/post.html',
     )
