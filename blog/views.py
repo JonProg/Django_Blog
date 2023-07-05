f964e1a4dc5c613dfe350f9d65bfc3ff6db51886
jon_prog <jonashenri.aquino@gmail.com>
2023-07-05 12:52:35 -0300

    starting to make the post page

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,44 @@ from django.shortcuts import render
 from django.core.paginator import Paginator
 from blog.models import Post
 
+
+PER_PAGE = 9
+
 def index(request):
     posts = Post.objects.get_published()
 
-    paginator = Paginator(posts, 9)
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+def created_by(request, author_id):
+    posts = Post.objects.get_published()\
+        .filter(created_by__id = author_id)
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+def category(request, slug):
+    posts = Post.objects.get_published().filter(category__slug=slug)
+
+    paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
@@ -23,8 +57,12 @@ def page(request):
         'blog/pages/page.html',
     )
 
-def post(request):
+def post(request, slug):
+    post = Post.objects.get_published().filter(slug=slug).first()
     return render(
         request,
         'blog/pages/post.html',
+        {
+            'post': post,
+        }
     )
