cb155c26421542cfcf5eacc1d54960b8d292d016
jon_prog <jonashenri.aquino@gmail.com>
2023-07-08 00:18:59 -0300

    add code for create pages in aplication

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from django.db.models import Q
-from blog.models import Post
+from blog.models import Post, Page
 
 
 PER_PAGE = 9
@@ -91,11 +91,21 @@ def search(request):
     )
 
 
-def page(request):
+def page(request, slug):
+    page = (
+        Page.objects
+        .filter(is_published = True)
+        .filter(slug=slug)
+        .first()
+    )
+
     return render(
         request,
         'blog/pages/page.html',
-    )
+        {
+            'page':page
+        }
+    )   
 
 def post(request, slug):
     post = Post.objects.get_published().filter(slug=slug).first()
