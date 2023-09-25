ed693b696e827ea78b9c5c28a0488e757463adf4
jon_prog <jonashenri.aquino@gmail.com>
2023-09-25 04:10:54 -0300

    modified project

--- project/urls.py
+++ project/urls.py
@@ -31,3 +31,10 @@ if settings.DEBUG:
         settings.MEDIA_URL,
         document_root = settings.MEDIA_ROOT
     )
+
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root = settings.STATIC_ROOT
+    )
+
+
